abcd43c8eeeccc28adc84d69b7c714fd9721b91a
nobody <nobody@ba19aa83-45c5-6ac9-afd3-db810772062c>
2005-02-19 00:06:33 +0000

    This commit was manufactured by cvs2svn to create tag 'postmerge-1_4-1_3_2-beta3'.

---
+++ tags/postmerge-1_4-1_3_2-beta3/src/classes/share/javax/media/j3d/ShaderProgram.java
@@ -0,0 +1,48 @@
+/*
+ * $RCSfile$
+ *
+ * Copyright (c) 2005 Sun Microsystems, Inc. All rights reserved.
+ *
+ * Use is subject to license terms.
+ *
+ * $Revision$
+ * $Date$
+ * $State$
+ */
+
+package javax.media.j3d;
+
+/**
+ * The ShaderProgram object is the abstract base class for
+ * programmable shader programs.  It contains no public methods,
+ * fields or constructors.  Each concrete instance of a ShaderProgram
+ * is a container for a set of Shader objects. The set of Shaders
+ * contained in the ShaderProgram is a complete program for the
+ * Graphics Pipeline Unit (GPU) of the graphics accelerator. It is
+ * specified using the shader language defined by the
+ * ShaderProgram. The currently defined shader languages are: Cg and
+ * GLSL.
+ *
+ * <p>
+ * NOTE: Applications should <i>not</i> extend this class.
+ *
+ * @see Shader
+ * @see ShaderAppearance#setShaderProgram
+ *
+ * @since Java 3D 1.4
+ */
+
+public abstract class ShaderProgram extends NodeComponent {
+
+    /**
+     * Package scope constructor so it can't be subclassed by classes
+     * outside the javax.media.j3d package.
+     */
+    ShaderProgram() {
+    }
+
+    /**
+     * Method to update the native shader program.
+     */
+    abstract void updateNative(long ctx);
+}
