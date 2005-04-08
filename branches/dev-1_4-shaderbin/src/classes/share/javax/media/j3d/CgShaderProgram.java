339d7df88534cc810587a4fb50cbeb145fcb5621
kcr <kcr@ba19aa83-45c5-6ac9-afd3-db810772062c>
2005-04-08 22:54:05 +0000

    Merged changes from root-1_4-shaderbin and chk-1_4_0-pre1-A (on the dev-1_4 branch) into this dev-1_4-shaderbin branch

--- branches/dev-1_4-shaderbin/src/classes/share/javax/media/j3d/CgShaderProgram.java
+++ branches/dev-1_4-shaderbin/src/classes/share/javax/media/j3d/CgShaderProgram.java
@@ -13,8 +13,8 @@
 package javax.media.j3d;
 
 /**
- * The CgShaderProgram object is a concrete implementation of a ShaderProgram
- * node component for NVIDIA's Cg shader language.
+ * The CgShaderProgram object is a concrete implementation of a
+ * ShaderProgram node component for NVIDIA's Cg shader language.
  *
  * @see CgVertexShader
  * @see CgFragmentShader
@@ -23,27 +23,112 @@ package javax.media.j3d;
  */
 
 public class CgShaderProgram extends ShaderProgram {
-    private CgVertexShader vertexShader = null;
-    private CgFragmentShader fragmentShader = null;
+    private SourceCodeShader vertexShader = null;
+    private SourceCodeShader fragmentShader = null;
 
+
+    /**
+     * Constructs a Cg shader program node component.
+     *
+     * <br>
+     * TODO: ADD MORE DOCUMENTATION HERE.
+     */
     public CgShaderProgram() {
     }
 
-    public void setVertexShader(CgVertexShader vertexShader) {
-	this.vertexShader = vertexShader;
+    /**
+     * Copies the specified array of shaders into this shader
+     * program. This method makes a shallow copy of the array. The
+     * array of shaders may be null or empty (0 length), but the
+     * elements of the array must be non-null. The shading language of
+     * each shader in the array must be
+     * <code>SHADING_LANGUAGE_CG</code>. Each shader in the array must
+     * be a SourceCodeShader. There must be no more than one vertex shader
+     * and one fragment shader in the array.
+     *
+     * @param shaders array of Shader objects to be copied into this
+     * ShaderProgram
+     *
+     * @exception CapabilityNotSetException if appropriate capability is
+     * not set and this object is part of live or compiled scene graph
+     *
+     * @exception IllegalArgumentException if the shading language of
+     * any shader in the shaders array is <em>not</em>
+     * <code>SHADING_LANGUAGE_CG</code>.
+     *
+     * @exception IllegalArgumentException if there are more than one
+     * vertex shader or more than one fragment shader in the shaders
+     * array.
+     *
+     * @exception ClassCastException if any shader in the shaders
+     * array is <em>not</em> a SourceCodeShader.
+     */
+    public void setShaders(Shader[] shaders) {
+	if (isLiveOrCompiled()) {
+	    if(!this.getCapability(ALLOW_SHADERS_WRITE)) {
+		throw new CapabilityNotSetException(J3dI18N.getString("CgShaderProgram0"));
+	    }
+	}
+
+	// TODO: move the rest of this into a CgShaderProgramRetained class
+
+	if (shaders == null) {
+	    vertexShader = fragmentShader = null;
+	    return;
+	}
+
+	// Check shaders for valid shading language, class type, etc.
+	for (int i = 0; i < shaders.length; i++) {
+	    boolean hasVertexShader = false;
+	    boolean hasFragmentShader = false;
+
+	    // Check shading language
+	    if (shaders[i].getShadingLanguage() != Shader.SHADING_LANGUAGE_CG) {
+		throw new IllegalArgumentException(J3dI18N.getString("CgShaderProgram2"));
+	    }
+
+	    // Check for more than one vertex shader or fragment shader
+	    if (shaders[i].getShaderType() == Shader.SHADER_TYPE_VERTEX) {
+		if (hasVertexShader) {
+		    throw new IllegalArgumentException(J3dI18N.getString("CgShaderProgram3"));
+		}
+		hasVertexShader = true;
+	    }
+	    else { // Shader.SHADER_TYPE_FRAGMENT
+		if (hasFragmentShader) {
+		    throw new IllegalArgumentException(J3dI18N.getString("CgShaderProgram4"));
+		}
+		hasFragmentShader = true;
+	    }
+
+	    // Try to cast shader to SourceCodeShader; it will throw
+	    // ClassCastException if it isn't.
+	    SourceCodeShader shad = (SourceCodeShader)shaders[i];
+	}
+
+	// Copy vertex and fragment shader
+	vertexShader = fragmentShader = null;
+	for (int i = 0; i < shaders.length; i++) {
+	    if (shaders[i].getShaderType() == Shader.SHADER_TYPE_VERTEX) {
+		vertexShader = (SourceCodeShader)shaders[i];
+	    }
+	    else { // Shader.SHADER_TYPE_FRAGMENT
+		fragmentShader = (SourceCodeShader)shaders[i];
+	    }
+	}
     }
 
-    public void setFragmentShader(CgFragmentShader fragmentShader) {
-	this.fragmentShader = fragmentShader;
+    // Implement abstract getShaders method (inherit javadoc from parent class)
+    public Shader[] getShaders() {
+	if (isLiveOrCompiled()) {
+	    if(!this.getCapability(ALLOW_SHADERS_READ)) {
+		throw new CapabilityNotSetException(J3dI18N.getString("CgShaderProgram1"));
+	    }
+	}
+
+	throw new RuntimeException("not implemented");
     }
 
-    public CgVertexShader getVertexShader() {
-	return vertexShader;
-    }
-
-    public CgFragmentShader getFragmentShader() {
-	return fragmentShader;
-    }
 
     private native void updateNative(long ctx,
 				     String vtxShader,
