f4c639f8b0005c8d51cf465a78e8e03faa646aee
nobody <nobody@ba19aa83-45c5-6ac9-afd3-db810772062c>
2005-11-24 00:33:56 +0000

    This commit was manufactured by cvs2svn to create branch 'dev-1_5'.

---
+++ branches/dev-1_5/src/classes/share/javax/media/j3d/ShaderAttributeRetained.java
@@ -0,0 +1,55 @@
+/*
+ * $RCSfile$
+ *
+ * Copyright (c) 2005 Sun Microsystems, Inc. All rights reserved.
+ *
+ * Use is subject to license terms.
+ *
+ * $Revision$
+ * $Date$
+ * $State$
+ */
+
+package javax.media.j3d;
+
+import javax.vecmath.*;
+
+/**
+ * The ShaderAttributeRetained object encapsulates a uniform attribute for a
+ * shader programs.
+ */
+
+abstract class ShaderAttributeRetained extends NodeComponentRetained {
+    // A list of pre-defined bits to indicate which component
+    // in this ShaderAttribute object changed.
+    static final int SHADER_ATTRIBUTE_VALUE_UPDATE            = 0x001;
+    
+    /**
+     * Name of the shader attribute (immutable)
+     */
+    String attrName;
+
+    /**
+     * Package scope constructor
+     */
+    ShaderAttributeRetained() {
+    }
+
+    void initializeAttrName(String attrName) {
+	this.attrName = attrName;
+    }
+
+    /**
+     * Retrieves the name of this shader attribute.
+     *
+     * @return the name of this shader attribute
+     */
+    String getAttributeName() {
+	return attrName;
+    }
+
+    void initMirrorObject() {
+	((ShaderAttributeObjectRetained)mirror).initializeAttrName(this.attrName);
+    }
+
+}
