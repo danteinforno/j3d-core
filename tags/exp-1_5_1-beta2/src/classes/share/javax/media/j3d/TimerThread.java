5f832e81680e15c1a10817ed21138f835761ab40
nobody <nobody@ba19aa83-45c5-6ac9-afd3-db810772062c>
2007-06-04 21:28:15 +0000

    This commit was manufactured by cvs2svn to create tag 'exp-1_5_1-beta2'.

---
+++ tags/exp-1_5_1-beta2/src/classes/share/javax/media/j3d/TimerThread.java
@@ -0,0 +1,152 @@
+/*
+ * $RCSfile$
+ *
+ * Copyright (c) 2007 Sun Microsystems, Inc. All rights reserved.
+ *
+ * Use is subject to license terms.
+ *
+ * $Revision$
+ * $Date$
+ * $State$
+ */
+
+package javax.media.j3d;
+
+/**
+ * The TimerThread is thread that handle WakeupOnElapsedTime call.
+ * There is only one thread for the whole system.
+ */
+
+class TimerThread extends Thread {
+
+    // action flag for runMonitor
+    private static final int WAIT   = 0;
+    private static final int NOTIFY = 1;
+    private static final int STOP   = 2;
+
+    private WakeupOnElapsedTimeHeap heap = new WakeupOnElapsedTimeHeap();
+
+    // Wakeup InputDeviceScheduler for every sample time reach
+    private WakeupOnElapsedTime inputDeviceSchedCond = 
+        new WakeupOnElapsedTime(InputDeviceScheduler.samplingTime);
+
+    // Wakeup {all?} Sound Scheduler{s} for every sample time reach
+    // QUESTION: this sampling time is set to a very large value so Sound
+    //          Schedulers are not pinged often unless explicitly requested
+    // XXXX: need a way to remove/null this condition when all
+    //          soundschedulers are halted
+    private WakeupOnElapsedTime soundSchedCond = 
+        new WakeupOnElapsedTime(120000);  // every 2 minutes
+
+    private volatile boolean running = true;
+    private boolean waiting = false;
+    private boolean ready = false;
+
+    TimerThread(ThreadGroup t) {
+	super(t, "J3D-TimerThread");
+    }
+
+    // call from UserThread
+    void add(WakeupOnElapsedTime wakeup) {
+	synchronized (heap) {
+	    heap.insert(wakeup);
+	}
+	runMonitor(NOTIFY, 0);
+    }
+
+    void addInputDeviceSchedCond() {
+	inputDeviceSchedCond.triggeredTime = 
+	    InputDeviceScheduler.samplingTime +
+	    J3dClock.currentTimeMillis();
+	add(inputDeviceSchedCond);
+    }
+
+    void addSoundSchedCond(long wakeupTime) {
+        // XXXX: there are potentially multiple sound schedulers.
+        //     this code will force a wait up on ALL sound schedulers
+        //     even though only one needs to process the sound that
+        //     this wakeup condition is triggered by.
+	soundSchedCond.triggeredTime = wakeupTime;
+	add(soundSchedCond);
+    }
+
+    // call from MasterThread
+    void finish() {
+	runMonitor(STOP, 0);
+    }
+
+    void remove(WakeupOnElapsedTime w) {
+	synchronized (heap) {
+	    heap.extract(w);
+	}
+    }
+
+    public void run() {
+	long waitTime = -1;
+	long time; 
+	WakeupOnElapsedTime cond;
+
+	while (running) {
+	    runMonitor(WAIT, waitTime);
+	    time = J3dClock.currentTimeMillis();
+
+	    while (true) {
+		cond = null;
+		waitTime = -1;
+		synchronized (heap) {
+		    if (!heap.isEmpty()) {
+			waitTime = heap.getMin().triggeredTime - time;
+			if (waitTime <= 0) {
+			    cond = heap.extractMin();
+			}
+		    }
+		}
+		if (cond == null) {
+		    break;
+		} else if (cond == inputDeviceSchedCond) {
+		    VirtualUniverse.mc.sendRunMessage(
+				      J3dThread.INPUT_DEVICE_SCHEDULER);
+		} else if (cond == soundSchedCond) {
+		    VirtualUniverse.mc.sendRunMessage(
+				      J3dThread.SOUND_SCHEDULER);
+		} else {
+		    cond.setTriggered();
+		}
+	    }
+	}
+    }
+
+
+    synchronized void runMonitor(int action, long waitTime) {
+	switch (action) {
+	case WAIT:
+            // Issue 308 - wait unless ready flag already set
+            // Note that we can't loop since we need to be able to timeout
+            // after "waitTime" msec
+            if (running && !ready) {
+                waiting = true;
+                try {
+                    if (waitTime < 0) {
+                        wait();
+                    } else {
+                        wait(waitTime);
+                    }
+                } catch (InterruptedException e) {}
+                waiting = false;
+            }
+            ready = false;
+            break;
+	case NOTIFY:
+            ready = true;
+            if (waiting) {
+                notify();
+            }
+	    break;
+	case STOP:
+	    running = false;
+	    notify();
+	    break;
+	}
+    }
+
+}
---
+++ tags/exp-1_5_1-beta2/src/native/d3d/GraphicsContext3D.cpp
@@ -0,0 +1,126 @@
+/*
+ * $RCSfile$
+ *
+ * Copyright (c) 2007 Sun Microsystems, Inc. All rights reserved.
+ *
+ * Use is subject to license terms.
+ *
+ * $Revision$
+ * $Date$
+ * $State$
+ */
+
+#include "StdAfx.h"
+
+extern "C" JNIEXPORT
+void JNICALL Java_javax_media_j3d_NativePipeline_readRaster(
+    JNIEnv *env, jobject obj, jlong ctx,
+    jint type, jint xOffset, jint yOffset, 
+    jint wRaster, jint hRaster, jint hCanvas,
+    jint imageDataType,
+    jint imageFormat, jobject imageBuffer,
+    jint depthFormat, jobject depthBuffer)
+{
+    void *imageObjPtr;
+    void *depthObjPtr;
+
+    GetDevice();
+
+    /* printf("[GraphicsContext3D] readRaster ...\n"); */
+    
+    if ((type & javax_media_j3d_Raster_RASTER_COLOR) != 0) {
+
+	if ((d3dCtx->d3dPresent.SwapEffect == D3DSWAPEFFECT_DISCARD) 
+	    // For offScreen rendering, swapBuffer never invoked
+	    // so it is safe to use backBuffer
+	    && (!d3dCtx->offScreen)
+	    // If fail to createFrontBuffer, fallback to use
+	    // backSurface. There is no gaurantee this fallback
+	    // will work, but at least in non-debug DirectX library
+	    // it works.
+	    && ((d3dCtx->frontSurface != NULL) ||
+		(d3dCtx->frontSurface == NULL) &&
+		d3dCtx->createFrontBuffer())) {
+	    
+	    
+	    HRESULT hr = device->GetFrontBufferData(0,d3dCtx->frontSurface);//iSwapChain as 0
+	    if (FAILED(hr)) {
+		    printf("GetFrontBuffer fail %s\n", DXGetErrorString9(hr));
+		    return;
+	    }
+	    imageObjPtr = (void *) env->GetPrimitiveArrayCritical((jarray)imageBuffer, NULL);
+	    
+	    if (!d3dCtx->bFullScreen) {
+		// We need to invoke GetWindowRect() everytime
+		// since message resize() will not receive
+		// when Canvas3D inside browers.
+		d3dCtx->getScreenRect(d3dCtx->hwnd, &d3dCtx->windowRect);
+		copyDataFromSurface(imageFormat, 
+				    xOffset + d3dCtx->windowRect.left, 
+				    yOffset + d3dCtx->windowRect.top, 
+				    wRaster,
+				    hRaster, 
+				    (jbyte *) imageObjPtr, 
+				    d3dCtx->frontSurface);
+	    } 
+	    else {
+		copyDataFromSurface(imageFormat, xOffset, yOffset,
+				    wRaster, hRaster,  (jbyte *) imageObjPtr,
+				    d3dCtx->frontSurface);
+	    }
+ 
+	    env->ReleasePrimitiveArrayCritical((jarray) imageBuffer, imageObjPtr, 0);
+
+	} 
+	else {
+	    if (d3dCtx->backSurface == NULL) {
+		HRESULT hr = device->GetBackBuffer(0,0, D3DBACKBUFFER_TYPE_MONO, //isSwapChain as 0
+						   &d3dCtx->backSurface);
+		if (FAILED(hr)) {
+		    printf("GetBackBuffer fail %s\n", DXGetErrorString9(hr));
+		    return;
+		}
+	    }
+	    imageObjPtr = (void *) env->GetPrimitiveArrayCritical((jarray)imageBuffer, NULL);
+
+	    copyDataFromSurface(imageFormat, xOffset, yOffset, wRaster,
+				hRaster, (jbyte *) imageObjPtr, d3dCtx->backSurface);
+
+	    env->ReleasePrimitiveArrayCritical((jarray)imageBuffer, imageObjPtr, 0);
+
+        }
+	
+    }
+
+    if ((type & javax_media_j3d_Raster_RASTER_DEPTH) != 0) {
+
+	if (d3dCtx->depthStencilSurface == NULL) {
+	    HRESULT hr =
+		device->GetDepthStencilSurface(&d3dCtx->depthStencilSurface);
+	    if (FAILED(hr)) {
+		if (debug) {
+		    printf("[Java3D] Fail to get depth stencil surface %s\n",
+			   DXGetErrorString9(hr));
+		}
+		return;
+	    }
+	}
+
+	depthObjPtr = (void *) env->GetPrimitiveArrayCritical((jarray)depthBuffer, NULL);
+
+        if (depthFormat == javax_media_j3d_DepthComponentRetained_DEPTH_COMPONENT_TYPE_INT) { 
+
+            // yOffset is adjusted for OpenGL - Y upward
+	    copyDepthFromSurface(xOffset, yOffset, wRaster,
+				 hRaster, (jint *) depthObjPtr, d3dCtx->depthStencilSurface);
+
+        } else { // javax_media_j3d_DepthComponentRetained_DEPTH_COMPONENT_TYPE_FLOAT
+	    
+            // yOffset is adjusted for OpenGL - Y upward
+	    copyDepthFromSurface(xOffset, yOffset, wRaster,
+				 hRaster, (jfloat *) depthObjPtr, d3dCtx->depthStencilSurface);
+        }
+	env->ReleasePrimitiveArrayCritical((jarray)depthBuffer, depthObjPtr, 0);
+    }
+}
+
